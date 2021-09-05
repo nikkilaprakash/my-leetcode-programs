f2ddfd69ec9d24d948c57d9e15b011c5e7e66912
NiKkIn <58518880+nikkilaprakash@users.noreply.github.com>
2021-09-05 16:36:18 +0530

    Add files via upload

---
+++ find-the-duplicate-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        while( i < n){
+            if( nums[i] == nums[i + 1])
+                return nums[i];
+            i++;
+        }
+        return -1;
+    }
+};
